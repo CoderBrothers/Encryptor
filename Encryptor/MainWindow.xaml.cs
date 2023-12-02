4739eec0cddfe894e7d0bbaa4f1a3db297451911
CoderBrothers <121336523+CoderBrothers@users.noreply.github.com>
2023-12-02 10:12:22 +0200

    Added start code

--- Encryptor/MainWindow.xaml.cs
+++ Encryptor/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,34 @@ namespace Encryptor
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private List<string> _paths = new();
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) DragMove();
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            sfd.Filter = "Encrypted files(*.enc)|*.enc";
+            if (sfd.ShowDialog() == false) return;
+            var writer = new StreamWriter(sfd.FileName);
+            foreach (var item in _paths)
+            {
+                writer.WriteLine(item);
+            }
+            writer.Close();
+            MessageBox.Show("Encrypted text saved");
         }
     }
 }
